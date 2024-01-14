c51967c189f39bfcba35103ee167bffdede853c8
Dzmitry Sudalenka <sudalenko@gmail.com>
2024-01-14 12:23:50 +0300

    feat: add read file task solution

---
+++ 01-read-file/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+const stream = fs.createReadStream(filePath);
+
+stream.on('data', (chunk) => console.log(chunk.toString()));
